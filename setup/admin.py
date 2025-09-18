12feb539c28e5e9274e9883f57fa9a06e82703ac
gpsf-01 <gpsf@cesar.school>
2025-09-18 20:53:24 -0300

    Criação das tabelas do banco

--- setup/admin.py
+++ setup/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from django.contrib import admin
+from .models import Linguagem, Assunto 
 # Register your models here.
+admin.site.register(Linguagem)
+admin.site.register(Assunto)
