789515cf16c49d1df7e776bac3daaf4752781183
guilhermebaltar <gcb@cesar.school>
2025-09-19 16:59:50 -0300

    criação url do quiz 2/6

--- setup/urls.py
+++ setup/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('configurar/', views.configurar_quiz, name='configurar_quiz'),
+    path('python/sintaxe-basica/', views.quiz_python_sb, name='python_sb'),
+    path('python/repeticao/', views.quiz_python_er, name='python_er'),
+
 ]
