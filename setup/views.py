5caca707996ba37aa6984a8e6a32705d36a4e28b
matheusfreire26 <mfpcavalcanti@gmail.com>
2025-09-19 17:25:27 -0300

    criação da função de sintaxe básica sobre a linguagem python

--- setup/views.py
+++ setup/views.py
@@ -22,3 +22,31 @@ def configurar_quiz(request):
         return render(request, 'quiz_gerado.html', {'linguagem': linguagem_id, 'assunto': assunto_id})
         
     return render(request, 'configurar_quiz.html', {'linguagens': linguagens, 'assuntos': assuntos})
+def python_sb(request):
+    try:
+        assunto_atual = Assunto.objects.get(nome__iexact="Sintaxe Básica", linguagem__nome__iexact="Python")
+    except Assunto.DoesNotExist:
+        messages.error(request, "O quiz de 'Sintaxe Básica de Python' não está configurado.")
+        return redirect('configurar_quiz')
+
+    perguntas = Pergunta.objects.filter(assunto=assunto_atual)
+
+    if not perguntas.exists():
+        messages.warning(request, "Ainda não há perguntas para 'Sintaxe Básica de Python'.")
+        return redirect('configurar_quiz')
+
+    if request.method == 'POST':
+        score = 0
+        total_perguntas = len(perguntas)
+        for pergunta in perguntas:
+            id_alternativa_selecionada = request.POST.get(f'pergunta_{pergunta.id}')
+            if id_alternativa_selecionada:
+                alternativa_correta = pergunta.alternativas.filter(is_correta=True).first()
+                if alternativa_correta and int(id_alternativa_selecionada) == alternativa_correta.id:
+                    score += 1
+        
+        contexto_resultado = {'score': score, 'total_perguntas': total_perguntas, 'assunto': assunto_atual}
+        return render(request, 'resultado_quiz.html', contexto_resultado)
+
+    context = {'perguntas': perguntas, 'assunto': assunto_atual}
+    return render(request, 'iniciar_quiz.html', context)
