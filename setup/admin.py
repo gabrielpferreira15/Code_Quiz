2d1aba6b8740d81a1968e6db7ac26de388bf99ee
gpsf-01 <gpsf@cesar.school>
2025-09-20 19:00:51 -0300

    Melhoria do admin e preenchimento automático de tabelas

--- setup/admin.py
+++ setup/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from django.contrib import admin
 from .models import Linguagem, Assunto, Pergunta, Resposta 
-# Register your models here.
+class RespostaInline(admin.TabularInline):
+    model = Resposta
+    extra = 4 
+class PerguntaAdmin(admin.ModelAdmin):
+    inlines = [RespostaInline]
+    list_display = ('texto', 'assunto') 
+
 admin.site.register(Linguagem)
 admin.site.register(Assunto)
-admin.site.register(Pergunta)
-admin.site.register(Resposta)
+admin.site.register(Pergunta, PerguntaAdmin)
