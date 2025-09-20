26f5832e5ea5454579d9a7d1e4da6076c54224d8
gpsf-01 <gpsf@cesar.school>
2025-09-20 19:24:18 -0300

    Correção de escrita/display de palavras no admin

--- setup/models.py
+++ setup/models.py
@@ -3,20 +3,26 @@ from django.db import models
 # Create your models here.
 class Linguagem(models.Model):
     nome = models.CharField(max_length=100)
-    
+    class Meta:
+        verbose_name_plural = "Linguagens"
+
     def __str__(self):
         return self.nome
 
 class Assunto(models.Model):
     nome = models.CharField(max_length=100)
     linguagem = models.ForeignKey(Linguagem, on_delete=models.CASCADE)
-    
+    class Meta:
+        verbose_name_plural = "Assuntos"
+
     def __str__(self):
         return f"{self.linguagem.nome}  -  {self.nome}"
 
 class Pergunta(models.Model):
     texto = models.TextField()
     assunto = models.ForeignKey(Assunto, on_delete=models.CASCADE)
+    class Meta:
+        verbose_name_plural = "Perguntas"
 
     def __str__(self):
         return self.texto[:50]
@@ -25,6 +31,8 @@ class Resposta(models.Model):
     pergunta = models.ForeignKey(Pergunta, on_delete=models.CASCADE, related_name="respostas")
     texto = models.CharField(max_length=255)
     correta = models.BooleanField(default=False)
+    class Meta:
+        verbose_name_plural = "Respostas"
 
     def __str__(self):
         return f"{self.texto} ({'Correta' if self.correta else 'Errada'})"
