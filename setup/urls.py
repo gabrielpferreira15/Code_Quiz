42a113241335e95e7bbde7ce4a72c90362c6df41
gpsf-01 <gpsf@cesar.school>
2025-09-28 14:17:41 -0300

    Mudança da url

--- setup/urls.py
+++ setup/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('configurar/', views.configurar_quiz, name='configurar_quiz'),
+    path('', views.configurar_quiz, name='configurar_quiz'),
     path('api/assuntos/<int:linguagem_id>/', views.get_assuntos, name='get_assuntos'),
     path('quiz/iniciar/<int:assunto_id>/', views.iniciar_novo_quiz, name='iniciar_novo_quiz'),
     path('quiz/jogar/<int:assunto_id>/', views.jogar_quiz, name='jogar_quiz'),
