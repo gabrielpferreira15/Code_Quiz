39f95f0219f6b11ab9768bea50dbac9a8471bd4f
gpsf-01 <gpsf@cesar.school>
2025-10-23 00:52:55 -0300

    Mudança nos startups e settings

--- startup_reset.sh
+++ startup_reset.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
+# Detectar o diretório correto da aplicação
+if [ -d "/tmp/8de11e555e54126" ]; then
+    APP_PATH="/tmp/8de11e555e54126"
+elif [ -d "/home/site/wwwroot" ]; then
+    APP_PATH="/home/site/wwwroot"
+else
+    APP_PATH="."
+fi
+
+cd $APP_PATH
+
+echo "========================================="
+echo "Diretório da aplicação: $APP_PATH"
+echo "========================================="
+
 # Ativar ambiente virtual
-source /home/site/wwwroot/antenv/bin/activate
+source $APP_PATH/antenv/bin/activate
 
 # RESET COMPLETO - Apagar todas as tabelas
 echo "========================================="
