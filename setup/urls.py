c8fd025e9f79d434b69cd1656a55f01ad779a3c6
gpsf-01 <gpsf@cesar.school>
2025-11-01 16:44:35 -0300

    Mudança dos caminhos de URL e das migrações

--- setup/urls.py
+++ setup/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('iniciar/<int:assunto_id>/<int:dificuldade_id>/', views.iniciar_novo_quiz, name='iniciar_novo_quiz'),
     path('api/dificuldades/', views.get_dificuldades, name='get_dificuldades'),
     path('jogar/<int:assunto_id>/', views.jogar_quiz, name='jogar_quiz'),
+    path('ranking/<int:assunto_id>/<int:dificuldade_id>/', views.ranking_quiz, name='ranking_quiz'),
 ]
